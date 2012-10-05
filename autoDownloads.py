8f69fa3e63c8ac160bdf354c7749c17ff7201d33
GoNZooo <rickard.m.andersson@gmail.com>
2012-10-05 13:50:43 +0200

    Added interval interpretation to the autodl-loader

--- autoDownloads.py
+++ autoDownloads.py
@@ -1,17 +1,61 @@
+# Conversion table for translation of human readable units.
+conversions = {
+                "m": 60,
+                "h": 60 * 60,
+                "d": 60 * 60 * 24,
+                "w": 60 * 60 * 24 * 7
+                }
+
 class CAutoDLItem:
-    def __init__(self, name, regex):
+    def __init__(self, name, regex, interval):
         self.name = name
         self.regex = regex
+        self.interval = interval
 
-def getAutoDownloads():
-    inFile = open("autodl.cfg", "r")
+    # Function to single out a given subterm and calculate the
+    # total time of that one unit.
+    def findHumanReadable(self, term, key):
+        pos = term.find(key)
+        if pos != -1:
+            coefficient = int(term[: pos])
+            return coefficient * conversions[key]
+        else:
+            return 0
+    
+    # Function to translate the human readable formats
+    # in the interval specification to seconds.
+    def translateHumanReadable(self, term):
+        totalTime = 0
+        totalTime += self.findHumanReadable(term, "w")
+        term = term[(term.find("w") + 1): ]
+        
+        totalTime += self.findHumanReadable(term, "d")
+        term = term[(term.find("d") + 1): ]
+        
+        totalTime += self.findHumanReadable(term, "h")
+        term = term[(term.find("h") + 1): ]
+        
+        totalTime += self.findHumanReadable(term, "m")
+        
+        return totalTime
+
+# Reading the autodl.cfg (or any other given file)
+# and the data necessary for a AutoDL item.
+def getAutoDownloads(filename = "autodl.cfg"):
+    inFile = open(filename, "r")
     autoDownloads = []
     for line in inFile:
-        tmpName = line[: line.find(";")]
-        tmpRegex = line[line.find(";") + 1:]
-        autoDownloads.append(CAutoDLItem(tmpName, tmpRegex))
-        inFile.close()
-
+        splitLine = line.split(";")
+        tmpName = splitLine[0]
+        tmpRegex = splitLine[1]
+        tmpInterval = splitLine[2].strip("\n")
+        autoDownloads.append(CAutoDLItem(tmpName, tmpRegex, tmpInterval))
+    inFile.close()
 
     return autoDownloads
 
+if __name__ == "__main__":
+    autoList = getAutoDownloads()
+    for item in autoList:
+        print(item.name, item.regex, item.interval,
+                item.translateHumanReadable(item.interval), sep="\t")
