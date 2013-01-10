706a974f7356effbb8e89315b5d594a9ca638121
GoNZooo <rickard.m.andersson@gmail.com>
2013-01-10 07:28:55 +0100

    Tabified the file for main-run and added optparse support

--- testRun.py
+++ testRun.py
@@ -4,43 +4,56 @@ import sys
 import autoDownloads
 import feedParser
 import pathcfg
+from optparse import OptionParser
 
-running = True
-downloaded = []
 
-if pathcfg.rssURL == "http://PUT/RSS/URL/HERE":
-    running = False
-    print("Default feed URL detected.")
-    print("You need to configure your RSS URL in the pathcfg.py file.")
 
-if len(sys.argv) > 1:
-    sleepTime = int(sys.argv[1])
-else:
-    sleepTime = 5 * 62
+if __name__ == "__main__":
+    parser = OptionParser()
 
-while running:
-    if not os.system(pathcfg.wget + " " + pathcfg.rssURL +  " -O current.xml"):
-        dataFile = open("current.xml", "r")
-        data = dataFile.read()
-        dataFile.close()
+    parser.add_option ("-s", "--sleep", action = "store", dest = "sleeptime",
+                        default = 5 * 62, type = "int",
+                        help = "How long the system should wait between GETs.")
+    parser.add_option ("-w", "--wget", action = "store", dest = "wget_path",
+                        default = pathcfg.wget, type = "string",
+                        help = "Path to wget.")
+    parser.add_option ("-r", "--rss", action = "store", dest = "rss_url",
+                        default = pathcfg.rssURL, type = "string",
+                        help = "URL to RSS feed.")
+    parser.add_option (""
 
-        channels = feedParser.separateChannels(data)
-        if channels:
-            itemStrings = feedParser.separateItems(channels[0])
-            itemContainers = feedParser.createItemContainers(itemStrings)
+    (options, args) = parser.parse_args()
+    running = True
+    downloaded = []
 
-            autoList = autoDownloads.getAutoDownloads()
-            for autoItem in autoList:
-                for item in itemContainers:
-                    if autoItem.matchRegex(item):
-                        if item.link not in downloaded:
-                            print("Match:", autoItem.name,
-                                autoItem.regex, item.title, sep = "\t")
-                            item.download()
-                            downloaded.append(item.link)
-    time.sleep(sleepTime)
-    loop += 1
-    if loop >= 400:
-        downloaded = []
-        loop = 0
+    if pathcfg.rssURL == "http://PUT/RSS/URL/HERE" and not options.rss_url:
+        running = False
+        print("Default feed URL detected.")
+        print("You need to configure your RSS URL in the pathcfg.py file.")
+
+    while running:
+        if not os.system(options.wget_path + " " + options.rss_url +  " -O current.xml"):
+            dataFile = open("current.xml", "r")
+            data = dataFile.read()
+            dataFile.close()
+
+            channels = feedParser.separateChannels(data)
+            if channels:
+                itemStrings = feedParser.separateItems(channels[0])
+                itemContainers = feedParser.createItemContainers(itemStrings)
+
+                autoList = autoDownloads.getAutoDownloads()
+                for autoItem in autoList:
+                    for item in itemContainers:
+                        if autoItem.matchRegex(item):
+                            if item.link not in downloaded:
+                                print("Match:", autoItem.name,
+                                    autoItem.regex, item.title, sep = "\t")
+                                item.download()
+                                downloaded.append(item.link)
+        time.sleep(options.sleeptime)
+        loop += 1
+        if loop >= 400:
+            downloaded = []
+            loop = 0
 
